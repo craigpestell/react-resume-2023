7e2e3482d2450d68fa270a0677f60f0beb9a05ad
Craig Pestell <craigpestell@gmail.com>
2023-07-02 23:38:10 -0700

    startOrReload

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,9 +15,10 @@ module.exports = {
       ref  : 'origin/main',
       repo : 'https://github.com/craigpestell/react-resume-2023.git',
       path : '/home/repn/web/resume',
-      'pre-deploy-local': 'yarn build',
+      //'pre-deploy-local': '',
       // 'post-deploy' : 'pm2 stop ecosystem.config.js resume && yarn && yarn build && pm2 start ecosystem.config.js resume',
       // 'pre-setup': ''
+      'post-deploy': 'pm2 startOrReload ecosystem.config.js production '
     }
   }
 };
