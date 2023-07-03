06828c0f56ad6cc9fe543773d936ac916ad57b31
Craig Pestell <craigpestell@gmail.com>
2023-07-02 23:29:44 -0700

    simple dir name

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     name: 'resume',
-    cwd: '/home/repn/work/react-resume-template',
+    cwd: '/home/repn/work/resume',
     script: 'yarn',
     args: 'start',
    // interpreter: '/bin/bash',
