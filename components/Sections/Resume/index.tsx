33eb2e531a3e3a639d0e55f7ace7863b93c9be0f
Craig Pestell <craigpestell@gmail.com>
2023-07-08 02:23:09 -0700

    incremental fixes, pm2

--- components/Sections/Resume/index.tsx
+++ components/Sections/Resume/index.tsx
@@ -8,38 +8,41 @@ import TimelineItem from './TimelineItem';
 
 const Resume: FC = memo(() => {
   return (
-    <Section className="bg-neutral-100" sectionId={SectionId.Resume}>
-      <div className="flex flex-col divide-y-2 divide-neutral-300">
-        <ResumeSection title="Education">
-          {education.map((item, index) => (
-            <TimelineItem item={item} key={`${item.title}-${index}`} />
-          ))}
-        </ResumeSection>
-        <ResumeSection title="Work">
-          {experience
-            .filter(item => item.location !== 'Remote')
-            .map((item, index) => (
+    <>
+      <Section sectionId={SectionId.Resume} />
+      <Section className="bg-neutral-100" sectionId={SectionId.Resume}>
+        <div className="flex flex-col divide-y-2 divide-neutral-300">
+          <ResumeSection title="Education">
+            {education.map((item, index) => (
               <TimelineItem item={item} key={`${item.title}-${index}`} />
             ))}
-        </ResumeSection>
-        <ResumeSection title="Freelance">
-          {experience
-            .filter(item => item.location === 'Remote')
-            .map(item => ({...item, location: ''}))
-            .map((item, index) => (
-              <TimelineItem item={item} key={`${item.title}-${index}`} />
-            ))}
-        </ResumeSection>
-        <ResumeSection title="Skills">
-          <p className="pb-8">A realistic self-appraisal of my skillset</p>
-          <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
-            {skills.map((skillgroup, index) => (
-              <SkillGroup key={`${skillgroup.name}-${index}`} skillGroup={skillgroup} />
-            ))}
-          </div>
-        </ResumeSection>
-      </div>
-    </Section>
+          </ResumeSection>
+          <ResumeSection title="Work">
+            {experience
+              .filter(item => item.location !== 'Remote')
+              .map((item, index) => (
+                <TimelineItem item={item} key={`${item.title}-${index}`} />
+              ))}
+          </ResumeSection>
+          <ResumeSection title="Freelance">
+            {experience
+              .filter(item => item.location === 'Remote')
+              .map(item => ({...item, location: ''}))
+              .map((item, index) => (
+                <TimelineItem item={item} key={`${item.title}-${index}`} />
+              ))}
+          </ResumeSection>
+          <ResumeSection title="Skills">
+            <p className="pb-8">A realistic self-appraisal of my skillset</p>
+            <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+              {skills.map((skillgroup, index) => (
+                <SkillGroup key={`${skillgroup.name}-${index}`} skillGroup={skillgroup} />
+              ))}
+            </div>
+          </ResumeSection>
+        </div>
+      </Section>
+    </>
   );
 });
 
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,23 +1,25 @@
 module.exports = {
-  apps : [{
-    name: 'resume',
-    cwd: '/home/repn/work/resume',
-    script: 'yarn',
-    args: 'start',
-    watch: false,
-  }],
+  apps: [
+    {
+      name: 'resume',
+      cwd: '/home/repn/work/resume',
+      script: 'yarn',
+      args: 'start',
+      watch: false,
+    },
+  ],
 
-  deploy : {
-    production : {
-      user : 'repn',
-      host : '24.199.76.78',
-      ref  : 'origin/main',
-      repo : 'https://github.com/craigpestell/react-resume-2023.git',
-      path : '/home/repn/web/resume',
-      //'pre-deploy-local': '',
-      // 'post-deploy' : 'pm2 stop ecosystem.config.js resume && yarn && yarn build && pm2 start ecosystem.config.js resume',
-      // 'pre-setup': ''
-      'post-deploy': 'pm2 startOrReload ecosystem.config.js production '
-    }
-  }
+  deploy: {
+    production: {
+      user: 'repn',
+      host: '24.199.76.78',
+      ref: 'origin/main',
+      repo: 'https://github.com/craigpestell/react-resume-2023.git',
+      path: '/home/repn/web/resume',
+      cwd: '/home/repn/web/resume',
+      'pre-deploy-local': 'yarn build && scp -r .next repn@24.199.76.78:/home/repn/web/resume/',
+      'pre-deploy': 'yarn build',
+      'post-deploy': 'pm2 startOrRestart ecosystem.config.js --env production',
+    },
+  },
 };
