df2a405f83c66dadad7858807e2ae1d2c57ffc3e
Craig Pestell <craigpestell@gmail.com>
2023-07-07 23:32:27 -0700

    better modal image

--- components/Sections/Portfolio.tsx
+++ components/Sections/Portfolio.tsx
@@ -1,13 +1,27 @@
+import 'react-medium-image-zoom/dist/styles.css';
+
 import {ArrowTopRightOnSquareIcon} from '@heroicons/react/24/outline';
 import classNames from 'classnames';
 import Image from 'next/image';
 import {FC, memo, MouseEvent, useCallback, useEffect, useRef, useState} from 'react';
+import Zoom from 'react-medium-image-zoom';
 
 import {isMobile} from '../../config';
 import {portfolioItems, SectionId} from '../../data/data';
 import {PortfolioItem} from '../../data/dataDef';
 import useDetectOutsideClick from '../../hooks/useDetectOutsideClick';
 import Section from '../Layout/Section';
+
+type Image = {
+  src: string;
+  alt: string;
+};
+
+export const ZoomImage = memo(({src, alt}: Image) => (
+  <Zoom>
+    <img alt={alt} className="hidden h-full w-full sm:block" src={src} />
+  </Zoom>
+));
 
 const Portfolio: FC = memo(() => {
   return (
@@ -23,8 +37,11 @@ const Portfolio: FC = memo(() => {
                   className={classNames(
                     'relative h-max w-full overflow-hidden rounded-lg shadow-lg shadow-black/30 lg:shadow-xl',
                   )}>
-                  <Image alt={title} className="h-full w-full" placeholder="blur" src={image} />
-                  <ItemOverlay item={item} />
+                  <ZoomImage alt={title} src={image.src} />
+                  <div className="block sm:hidden">
+                    <Image alt={title} className="h-full w-full" placeholder="blur" src={image} />
+                    <ItemOverlay item={item} />
+                  </div>
                 </div>
               </div>
             );
@@ -62,16 +79,14 @@ const ItemOverlay: FC<{item: PortfolioItem}> = memo(({item: {url, title, descrip
   );
   const href = url ? {href: url} : {};
   return (
-    <a
+    <button
       className={classNames(
         'absolute inset-0 h-full w-full  bg-gray-900 transition-all duration-300',
         {'opacity-0 hover:opacity-80': !mobile},
         showOverlay ? 'opacity-80' : 'opacity-0',
       )}
       {...href}
-      onClick={handleItemClick}
-      ref={linkRef}
-      target="_blank">
+      onClick={handleItemClick}>
       <div className="relative h-full w-full p-4">
         <div className="flex h-full w-full flex-col gap-y-2 overflow-y-auto overscroll-contain">
           <h2 className="text-center font-bold text-white opacity-100">{title}</h2>
@@ -79,6 +94,6 @@ const ItemOverlay: FC<{item: PortfolioItem}> = memo(({item: {url, title, descrip
         </div>
         <ArrowTopRightOnSquareIcon className="absolute bottom-1 right-1 h-4 w-4 shrink-0 text-white sm:bottom-2 sm:right-2" />
       </div>
-    </a>
+    </button>
   );
 });
