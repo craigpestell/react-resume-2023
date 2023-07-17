74a3517d4b72a40c44aa889aaec66dc42af42c8f
Craig Pestell <craigpestell@gmail.com>
2023-07-17 03:58:33 -0700

    linting

--- components/Sections/Contact/ContactForm.tsx
+++ components/Sections/Contact/ContactForm.tsx
@@ -84,7 +84,7 @@ const ContactForm: FC = memo(() => {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
-          },  
+          },
           body: JSON.stringify(data),
         });
 
