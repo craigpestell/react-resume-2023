e5a386e16a0d956ab0c1ce80c4104e4ca0473acc
Craig Pestell <craigpestell@gmail.com>
2024-02-11 23:51:22 -0800

    fix sending contact us email

--- components/Sections/Contact/ContactForm.tsx
+++ components/Sections/Contact/ContactForm.tsx
@@ -1,69 +1,46 @@
-import {FC, memo, useCallback, useMemo, useState} from 'react';
-
-interface FormData {
-  name: string;
-  email: string;
-  message: string;
-}
+import emailjs from '@emailjs/browser';
+import {FC, memo, useRef, useState} from 'react';
 
 const ContactForm: FC = memo(() => {
-  const defaultData = useMemo(
-    () => ({
-      name: '',
-      email: '',
-      message: '',
-    }),
-    [],
-  );
+  const form = useRef(null);
 
-  const [data, setData] = useState<FormData>(defaultData);
+  const [emailSent, setEmailSent] = useState<boolean>(false);
 
-  const onChange = useCallback(
-    <T extends HTMLInputElement | HTMLTextAreaElement>(event: React.ChangeEvent<T>): void => {
-      const {name, value} = event.target;
+  const sendEmail = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    form.current &&
+      emailjs.sendForm('service_xm9bqxy', 'template_uq22k5j', form.current, 'CQgadqY444YV2lQI8').then(
+        result => {
+          console.log(result.text);
+          console.log('message sent!');
+          setEmailSent(true);
+        },
+        error => {
+          console.log(error.text);
+          console.log('error sending message, try again!');
+        },
+      );
+  };
 
-      const fieldData: Partial<FormData> = {[name]: value};
-
-      setData({...data, ...fieldData});
-    },
-    [data],
-  );
-
-  const handleSendMessage = useCallback(
-    async (event: React.FormEvent<HTMLFormElement>) => {
-      event.preventDefault();
-      /**
-       * This is a good starting point to wire up your form submission logic
-       * */
-      console.log('Data to send: ', data);
-    },
-    [data],
-  );
+  const EmailSent = <p>Thanks for reaching out. I'll get back to you as soon as I can.</p>;
 
   const inputClasses =
     'bg-neutral-700 border-0 focus:border-0 focus:outline-none focus:ring-1 focus:ring-purple-600 rounded-md placeholder:text-neutral-400 placeholder:text-sm text-neutral-200 text-sm';
 
-  return (
-    <form className="grid min-h-[320px] grid-cols-1 gap-y-4" method="POST" onSubmit={handleSendMessage}>
-      <input className={inputClasses} name="name" onChange={onChange} placeholder="Name" required type="text" />
+  return emailSent ? (
+    EmailSent
+  ) : (
+    <form className="grid min-h-[320px] grid-cols-1 gap-y-4" method="POST" onSubmit={sendEmail} ref={form}>
+      <input className={inputClasses} name="user_name" placeholder="Name" required type="text" />
       <input
         autoComplete="email"
         className={inputClasses}
-        name="email"
-        onChange={onChange}
+        name="user_email"
         placeholder="Email"
         required
         type="email"
       />
-      <textarea
-        className={inputClasses}
-        maxLength={250}
-        name="message"
-        onChange={onChange}
-        placeholder="Message"
-        required
-        rows={6}
-      />
+      <textarea className={inputClasses} maxLength={250} name="user_message" placeholder="Message" required rows={6} />
       <button
         aria-label="Submit contact form"
         className="w-max rounded-full border-2 border-purple-600 bg-stone-900 px-4 py-2 text-sm font-medium text-white shadow-md outline-none hover:bg-stone-800 focus:ring-2 focus:ring-purple-600 focus:ring-offset-2 focus:ring-offset-stone-800"
